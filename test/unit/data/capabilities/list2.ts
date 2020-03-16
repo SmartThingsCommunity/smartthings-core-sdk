2d0fcf2796e2f451961dd2b86e401062a452eeb8
Jake Stark <jake.stark@smartthings.com>
2020-03-16 13:34:59 -0500

    list namespaces, standard capabilities, custom capabilities unit tests

--- test/unit/data/capabilities/list2.ts
+++ test/unit/data/capabilities/list2.ts
@@ -2,12 +2,12 @@ const data = {
 	items: [
 		{
 			id: 'switch',
-			version: 1,
+			version: 2,
 		},
 	],
 	'_links': {
 		'previous': {
-			'href': 'https://api.smartthings.com/capabilities?page=0&max=200',
+			'href': 'https://api.smartthings.com/capabilities/namespace/testNamespace?page=0&max=200',
 		},
 	},
 }
