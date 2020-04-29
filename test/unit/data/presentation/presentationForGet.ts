5a22fdb37500acf0e5d4356c8a218f681b9b194e
Ross Stenersen <rossiam@gmail.com>
2020-04-29 13:34:08 -0500

    add support for presentation endpoints

---
+++ test/unit/data/presentation/presentationForGet.ts
@@ -0,0 +1,9 @@
+import { PresentationDeviceConfigCreate } from '../../../../src'
+
+
+const data = {
+	'mnmn': 'Test Manufacturer',
+	'vid': 'vendor-00',
+} as PresentationDeviceConfigCreate
+
+export default data
---
+++ test/unit/presentation.test.ts
@@ -0,0 +1,71 @@
+import axios from '../../__mocks__/axios'
+
+import { NoOpAuthenticator, SmartThingsClient, PresentationDeviceConfig } from '../../src'
+import presentationDeviceConfiguration from './data/presentation/presentation'
+import presentationDeviceConfigurationForGet from './data/presentation/presentationForGet'
+
+
+const authenticator = new NoOpAuthenticator()
+const client = new SmartThingsClient(authenticator, {})
+
+const profileId = '0000-0000-0000-0000'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function expectedRequest(path?: string, params?: any, data?: any, method = 'get'): any {
+	return {
+		url: `https://api.smartthings.com/${path}`,
+		method: method,
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Accept': 'application/json',
+		},
+		data: data,
+		params: params,
+		paramsSerializer: expect.anything(),
+	}
+}
+
+describe('Presentation',  () => {
+	afterEach(() => {
+		axios.request.mockReset()
+	})
+
+	it('generate', async () => {
+		axios.request.mockImplementationOnce(() => Promise.resolve({status: 200, data: presentationDeviceConfiguration}))
+		const response: PresentationDeviceConfig = await client.presentation.generate(profileId)
+
+		expect(axios.request).toHaveBeenCalledTimes(1)
+		expect(axios.request).toHaveBeenCalledWith(expectedRequest(`presentation/types/${profileId}/deviceconfig`, undefined))
+		expect(response).toBe(presentationDeviceConfiguration)
+	})
+
+	it('generate with optional params', async () => {
+		axios.request.mockImplementationOnce(() => Promise.resolve({status: 200, data: presentationDeviceConfiguration}))
+		const response: PresentationDeviceConfig = await client.presentation.generate(profileId,
+			{ typeIntegration: 'dth' })
+
+		expect(axios.request).toHaveBeenCalledTimes(1)
+		expect(axios.request).toHaveBeenCalledWith(expectedRequest(`presentation/types/${profileId}/deviceconfig`,
+			{ typeIntegration: 'dth' }))
+		expect(response).toBe(presentationDeviceConfiguration)
+	})
+
+	it('get', async () => {
+		axios.request.mockImplementationOnce(() => Promise.resolve({status: 200, data: presentationDeviceConfiguration}))
+		const response: PresentationDeviceConfig = await client.presentation.get('vid')
+
+		expect(axios.request).toHaveBeenCalledTimes(1)
+		expect(axios.request).toHaveBeenCalledWith(expectedRequest('presentation/deviceconfig', { vid: 'vid' }))
+		expect(response).toBe(presentationDeviceConfiguration)
+	})
+
+	it('create', async () => {
+		axios.request.mockImplementationOnce(() => Promise.resolve({status: 200, data: presentationDeviceConfiguration}))
+		const response: PresentationDeviceConfig = await client.presentation.create(presentationDeviceConfiguration)
+
+		expect(axios.request).toHaveBeenCalledTimes(1)
+		expect(axios.request).toHaveBeenCalledWith(expectedRequest('presentation/deviceconfig',
+			undefined, presentationDeviceConfiguration, 'post'))
+		expect(response).toBe(presentationDeviceConfiguration)
+	})
+})
