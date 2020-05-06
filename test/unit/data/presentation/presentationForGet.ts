e2a9b217e68b65858efa9739d8c28e585380ab52
Ross Stenersen <rossiam@gmail.com>
2020-05-06 14:40:21 -0500

    fix property case on iconUrl property name

--- test/unit/data/presentation/presentationForGet.ts
+++
@@ -1,9 +0,0 @@
-import { PresentationDeviceConfigCreate } from '../../../../src'
-
-
-const data = {
-	'mnmn': 'Test Manufacturer',
-	'vid': 'vendor-00',
-} as PresentationDeviceConfigCreate
-
-export default data
