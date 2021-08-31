58b1f6258c3ca96410b0d12b8f1969a37780bb92
Bob Florian <bob@smartthings.com>
2021-08-31 15:21:14 -0400

    feat: Added command to regenerate schema oauth credentials

--- test/unit/data/schema/post.ts
+++ test/unit/data/schema/post.ts
@@ -27,3 +27,23 @@ export const post_schema_apps = {
 		'stClientSecret': '51d713160873792a7ceacda72125128abea8965b0c18176304371396e2d76fbf565402c66845c33a32fdaf912a44aa9fffc50c4186961229ca3a3f6f3fc75a8ccd27bba72a69ec66028d985fb8111bfce9e4bbcf67cda6310eb7414e4ddac3ebb2a0052890d3ed8a543c094832ccbfe3bdde53a428a5709a0efdd8a25c15312a7307860d97cf216e3e0433dff99f7babc9ada40266ba35172c05ffc505f1fc11cf8c2e41d5b2c13668bc1013029b8885138b8ffa5ef16dcce3cb81efe2cdf9db092d06511bc0ff7ab41af5d42f6483723938487bff15ef3efe9adec21201fcddcb434472cbf71249ae43f5cd9d053a2d598ce00efc7727301729b12575629844',
 	},
 }
+
+export const post_oauth_update = {
+	'request': {
+		'url': 'https://api.smartthings.com/schema/oauth/stclient/credentials',
+		'method': 'post',
+		'headers': {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Accept': 'application/json',
+			'Authorization': 'Bearer 00000000-0000-0000-0000-000000000000',
+		},
+		'data': {
+			'endpointAppId': 'viper_9e767550-5f2a-11ea-9ea0-bb3ce8866e53',
+		},
+	},
+	'response': {
+		'endpointAppId': 'viper_9e767550-5f2a-11ea-9ea0-bb3ce8866e53',
+		'stClientId': 'aabdc28c-7f72-4380-a419-aeb52b27e0b4',
+		'stClientSecret': '51d713160873792a7ceacda72125128abea8965b0c18176304371396e2d76fbf565402c66845c33a32fdaf912a44aa9fffc50c4186961229ca3a3f6f3fc75a8ccd27bba72a69ec66028d985fb8111bfce9e4bbcf67cda6310eb7414e4ddac3ebb2a0052890d3ed8a543c094832ccbfe3bdde53a428a5709a0efdd8a25c15312a7307860d97cf216e3e0433dff99f7babc9ada40266ba35172c05ffc505f1fc11cf8c2e41d5b2c13668bc1013029b8885138b8ffa5ef16dcce3cb81efe2cdf9db092d06511bc0ff7ab41af5d42f6483723938487bff15ef3efe9adec21201fcddcb434472cbf71249ae43f5cd9d053a2d598ce00efc7727301729b12575629844',
+	},
+}
